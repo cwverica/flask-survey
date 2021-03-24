4bb0cebbb51d4a65449952a4216188d90fb1712c
cwverica <cwverica@gmail.com>
2021-03-24 00:04:42 -0700

    uploading exercise

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from surveys import satisfaction_survey
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, render_template, jsonify, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "supr-rly-secrt"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -14,26 +14,29 @@ survey = satisfaction_survey
 
 @app.route('/')
 def show_home():
-    app.responses = []
+    # Used this as a way to not have to restart the "server" everytime
+    responses.clear()
     return render_template('home.html')
 
 
 @app.route('/survey/<ident>')
 def show_survey(ident):
 
+    if int(ident) != len(responses):
+        return courseCorrect()
+
     if request.args.get('answers', None):
         responses.append(request.args['answers'])
     
-    title = survey.title
-    instructions = survey.instructions
     question_obj = survey.questions[int(ident)]
-    last = len(survey.questions) - 1
-    question = question_obj.question
-    choices = question_obj.choices
-    allow_text = question_obj.allow_text
-    index = int(ident)
-    page_dict = {'title': title, 'instructions': instructions, 'question': question,
-                    'choices': choices, 'allow_text': allow_text, 'index': index, 'last': last}
+
+    page_dict = {'title': survey.title,
+                'instructions': survey.instructions,
+                'question': question_obj.question,
+                'choices': question_obj.choices, 
+                'allow_text': question_obj.allow_text, 
+                'index': int(ident), 
+                'last': len(survey.questions) -1}
     return render_template('survey.html', page_dict=page_dict)
 
 @app.route('/answer', methods=["POST"])
@@ -44,11 +47,16 @@ def receive_answer():
         return redirect('survey/end', code=302)
     else:
         return redirect(f'survey/{next_page}', code=302)
-    # Something is happening where I can't get to the end page, the submit button just doesn't work on the last question
-
-
+    
 @app.route('/survey/end')
 def show_results():
     title = survey.title
     return render_template('end_page.html', title=title, responses=responses)
 
+def courseCorrect():
+    if len(responses) < len(survey.questions):
+        flash("You must answer the questions in order. You have been redirected.")
+        return redirect(f'{len(responses)}', code=302)
+    else:
+        flash("You have already completed the survey. You have been redirected.")
+        return redirect('end', code=302)
